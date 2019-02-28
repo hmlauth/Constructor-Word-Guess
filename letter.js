26a3667a87e4de3583df9f886a89ac46e3ad23aa
Andromeda <hannalauth@gmail.com>
2019-02-27 18:55:27 -0800

    files created

---
+++ letter.js
@@ -0,0 +1,9 @@
+function Letter() {
+    this.placeholder = "",
+    this.foundLetter = false,
+    this.checkForLetter = function(letter) {
+        
+    }
+}
+
+// This constructor function's purpose is to either display a placeholder character or the letter of the word if guessed correctly. 
