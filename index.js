a2439a02fc09698a3efede0d83e02ea830b6346e
Andromeda <hannalauth@gmail.com>
2019-02-28 16:30:34 -0800

    rough draft of letter.js constructor function

---
+++ index.js
@@ -0,0 +1,13 @@
+// inquirer
+//   .prompt([
+    
+//     type: "confirm",
+//     message: "Guess a letter!",
+//     name: "confirm-letter",
+//     validate = function() {
+
+//     }
+//   ])
+//   .then(answers => {
+//     // Use user feedback for... whatever!!
+//   });
--- letter.js
+++ letter.js
@@ -1,9 +1,39 @@
-function Letter() {
-    this.placeholder = "",
+var inquirer = require('inquirer');
+
+// **** use .toString() function.
+
+// Constructor function that will display underlying character or placeholder depending on whether or not the user has guessed the correct letter. 
+function Letter(letterToMatch) {
+    // A string value to store the underlying character for the letter being guessed
+    this.letterToMatch = "h",
+    // A boolean value that stores whether that letter has been guessed yet
+    // TRUE - If letter HAS BEEN guessed (correctly).
+    // FALSE - If letter HAS NOT been guessed yet.
     this.foundLetter = false,
+    // A function that returns the underlying character if the letter has been guessed, or a placeholder if the letter has not been guessed. 
+    this.displayCorrectLetter = function(letterToMatch) {
+        var guessedWord = [];
+        for (var i = 0; i < wordToMatch.length; i ++) {
+            if (letterToMatch === wordToMatch[i]) {
+                guessedWord[i] = letterToMatch; 
+            } else if (letterToMatch !== wordToMatch[i]) {
+                guessedWord[i] = (" _");
+            }
+                if (guessedWord.toString() === wordToMatch) {
+                    console.log(wordToMatch);
+                    console.log("You won! Great job!");
+                }
+
+        }
+    }
+    // A function that takes in a character as an argument and checks it against an underlying character, updating the stored boolean value to true if it was guessed correctly
     this.checkForLetter = function(letter) {
-        
+        for (var i = 0; i < wordToMatch.length; i++) {
+            if (letterToMatch === wordToMatch[i]) {
+                foundLetter = true;
+            }
+        }
     }
 }
 
-// This constructor function's purpose is to either display a placeholder character or the letter of the word if guessed correctly. 
+module.exports = Letter;
